0bd4a0fcd40ba672c564be945cfd02223aabfd0a
TATViet <153576362+TATViet@users.noreply.github.com>
2024-08-23 09:00:34 +0700

    Update cart.js

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/Payment");
+const Product = require("../models/Product");
 
 // Giả lập dữ liệu của cart (menu)
 let cart = [
